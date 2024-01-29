22516282ae32e2bf47542ba815fadf1920f8f76c
SyberiaCMAZ <michal.chwatko@gmail.com>
2024-01-29 18:35:20 +0100

    version 2

--- shinden/items.py
+++ shinden/items.py
@@ -7,6 +7,47 @@ import scrapy
 
 
 class ShindenItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
     pass
+
+class AnimeItem(scrapy.Item):
+    name = scrapy.Field()
+    description = scrapy.Field()
+    tags = scrapy.Field()
+    score = scrapy.Field()
+    score_votes = scrapy.Field()
+    story_score = scrapy.Field()
+    animation_score = scrapy.Field()
+    music_score = scrapy.Field()
+    character_score = scrapy.Field()
+    genre = scrapy.Field()
+    status = scrapy.Field()
+    start_date = scrapy.Field()
+    end_date = scrapy.Field()
+    episodes = scrapy.Field()
+    studio = scrapy.Field()
+    lenght = scrapy.Field()
+    mpaa = scrapy.Field()
+    watching = scrapy.Field()
+    watched = scrapy.Field()
+    skipped = scrapy.Field()
+    dropped = scrapy.Field()
+    stopped = scrapy.Field()
+    will_watch = scrapy.Field()
+    likes = scrapy.Field()
+    character = scrapy.Field()
+    cast = scrapy.Field()
+    comment = scrapy.Field()
+
+class CharacterItem(scrapy.Item):
+    name = scrapy.Field()
+    actor = scrapy.Field()
+
+class CastsItem(scrapy.Item):
+    name = scrapy.Field()
+    role = scrapy.Field()
+    
+
+class CommentItem(scrapy.Item):
+    user = scrapy.Field()
+    content = scrapy.Field()
+    date = scrapy.Field()
--- shinden/spiders/shinden_spider.py
+++ shinden/spiders/shinden_spider.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+from shinden.items import AnimeItem, CharacterItem, CastsItem, CommentItem
 import scrapy
 
 
@@ -12,16 +12,76 @@ class ShindenSpider(scrapy.Spider):
 
     def parse(self, response):
         for anime in response.xpath('//section[@class="title-table"]/article/ul'):
-            yield {
-                "name": anime.xpath('.//li[@class="desc-col"]/h3/a/text()').get(),
-                "tags": anime.xpath('.//ul[@class="tags"]/li/a/text()').getall(),
-                "type": anime.xpath('.//li[@class="title-kind-col"]/text()').get(),
-                "episodes": anime.xpath('.//li[@class="episodes-col"]/text()').get(),
-                "ratings": anime.xpath('.//li[@class="ratings-col"]/div/span/text()').getall(),
-                "status": anime.xpath('.//li[@class="title-status-col"]/text()').get(),
-                "ratetop": anime.xpath('.//li[@class="rate-top"]/text()').get(),
-            }
+            relative_url = anime.xpath('.//li/h3/a/@href').get()
+            yield response.follow(relative_url, callback=self.parse_anime_page)
         next_page = response.xpath('//a[@rel="next"]/@href').get()
 
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
+
+    def parse_anime_page(self, response):
+
+        character_details = response.xpath('//div[@class="ch-st-item"]')
+        character_items = []
+
+        for character in character_details:
+            character_item = CharacterItem(
+                name=character.xpath('span[1]/div/h3/a/text()').get(),
+                actor=character.xpath('span[2]/div/h3/a/text()').get(),
+            )
+            character_items.append(dict(character_item))
+
+        casts = response.xpath('//div[@class="person-character-text"]')
+        casts_items = []
+
+        for cast in casts:
+            casts_item = CastsItem(
+                name=cast.xpath('h3/a/text()').get(),
+                role=cast.xpath('p/text()').get(),
+            )
+            casts_items.append(dict(casts_item))
+
+        comments = response.xpath('//ul[@class="posts"]/li')
+        comment_items = []
+        for comment in comments:
+            comment_item = CommentItem(
+                user=comment.xpath('summary/strong/text()').get(),
+                content=comment.xpath('div/text()').get(),
+                date=comment.xpath('details/span/text()').get(),
+            )
+            comment_items.append(dict(comment_item))
+
+        anime_details = AnimeItem()
+        anime_details["name"] = response.xpath("//h1/span[2]/text()").get()
+        anime_details["description"] = response.xpath('//div[@id="description"]/p/text()').get()
+        anime_details["tags" ] = response.xpath('//ul[@class="tags"]/li/a/text()').getall()
+        anime_details["score" ] = response.xpath('//div/h3/span/text()').get()
+        anime_details["score_votes" ] = response.xpath('substring-before(//span[@class="h6"], "głosów")').get()
+        anime_details["story_score" ] = response.xpath('//ul[@class="info-aside-overall-rating"]/li[1]/text()').get()
+        anime_details["animation_score" ] = response.xpath('//ul[@class="info-aside-overall-rating"]/li[2]/text()').get()
+        anime_details["music_score" ] = response.xpath('//ul[@class="info-aside-overall-rating"]/li[3]/text()').get()
+        anime_details["character_score" ] = response.xpath('//ul[@class="info-aside-overall-rating"]/li[4]/text()').get()
+        anime_details["character_score" ] = response.xpath('//ul[@class="info-aside-overall-rating"]/li[4]/text()').get()
+        anime_details["genre"] = response.xpath("//dl[@class='info-aside-list']/dd[1]/text()").get()
+        anime_details["status"] = response.xpath("//dl[@class='info-aside-list']/dd[2]/text()").get()
+        anime_details["start_date"] = response.xpath("//dl[@class='info-aside-list']/dd[3]/text()").get()
+        anime_details["end_date"] = response.xpath("//dl[@class='info-aside-list']/dd[4]/text()").get()
+        anime_details["episodes"] = response.xpath("//dl[@class='info-aside-list']/dd[5]/text()").get()
+        anime_details["studio"] = response.xpath("//dl[@class='info-aside-list']/dd[6]/a/text()").getall()
+        anime_details["lenght"] = response.xpath("//dl[@class='info-aside-list']/dd[7]/text()").get()
+        anime_details["mpaa"] = response.xpath("//dl[@class='info-aside-list']/dd[8]/text()").get()
+        anime_details["watching"] = response.xpath("//section[@class='title-stats']/dl/dd[1]/text()").get()
+        anime_details["watched"] = response.xpath("//section[@class='title-stats']/dl/dd[2]/text()").get()
+        anime_details["skipped"] = response.xpath("//section[@class='title-stats']/dl/dd[3]/text()").get()
+        anime_details["dropped"] = response.xpath("//section[@class='title-stats']/dl/dd[4]/text()").get()
+        anime_details["stopped"] = response.xpath("//section[@class='title-stats']/dl/dd[5]/text()").get()
+        anime_details["will_watch"] = response.xpath("//section[@class='title-stats']/dl/dd[6]/text()").get()
+        anime_details["likes"] = response.xpath("//section[@class='title-stats']/dl/dd[7]/text()").get()
+        anime_details['character'] = character_items
+        anime_details['cast'] = casts_items
+        anime_details['comment'] = comment_items
+        
+
+
+
+        yield anime_details
